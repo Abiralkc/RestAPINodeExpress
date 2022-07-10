eb0445ce6d7f68d29f28c922bf10b27c76d26f22
Abiral <abiralkc21@gmail.com>
2022-07-10 17:14:25 +0545

    Update Request

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 import {v4 as uuidv4} from 'uuid';
 
 const router = express.Router();
@@ -38,5 +38,19 @@ router.delete('/:id',(req,res) =>{
 
     res.send(`User with the id ${id} deleted from the db`);
 });
+
+router.patch('/:id',(req,res)=>{
+    const {id} = req.params;
+    const {firstName,lastName,age} = req.body;
+
+    const user = users.find((user) => user.id ===id);
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+    
+    res.send(`User with the id ${id} has been updated`);
+
+})
 
 export default router;
