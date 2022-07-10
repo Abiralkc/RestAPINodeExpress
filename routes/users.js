aebff8ba82bd58fca0f0aeb3e52150b42957b5d5
Abiral <abiralkc21@gmail.com>
2022-07-10 16:53:39 +0545

    Get wit Id and Delete Done

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,10 @@
 import express from 'express';
+import {v4 as uuidv4} from 'uuid';
 
 const router = express.Router();
 
-const users =[
-    {
-        firstname:"John",
-        lastName:"Doe",
-        age:25
-    },
-    {
-        firstname:"Jane",
-        lastName:"Doe",
-        age:24
-
-    }
+let users =[
+  
 ]
 
 
@@ -26,9 +17,26 @@ router.get('/',(req,res)=>{
 router.post('/',(req,res) =>{
     const user = req.body;
 
-    users.push(user);
+    users.push({ ...user,id:uuidv4()});
 
     res.send(`USER with the name ${user.firstName} added to the database`);
 })
+
+router.get('/:id',(req,res) =>{
+
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id ===id);
+
+    res.send(foundUser);
+
+});
+
+router.delete('/:id',(req,res) =>{
+    const {id} = req.params;
+    users = users.filter((user)=> user.id !== id);
+
+    res.send(`User with the id ${id} deleted from the db`);
+});
 
 export default router;
